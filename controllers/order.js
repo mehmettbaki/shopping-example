0e963ec5ab6a21ba6ddd4a2ffa665326129751f4
mehmettbaki <mehmetbaki123@gmail.com>
2024-01-01 16:24:37 +0300

    delete mock

--- controllers/order.js
+++ controllers/order.js
@@ -1,44 +1,32 @@
 const { Order, User } = require('../db');
 
+
+// kullanıcının  bütün orderları listelenir
+// userid TOKENDAN ALINACAK
 const getAllOrders = async (req, res) => {
     try {
         const [order, _] = await Order.findAll({
             where: {
                 UserId: 'e9213d32-a32e-46f5-a085-6b513129fce1',
-                onay: false,
+                onay: true,
+                
             },
             include: 'Products',
         });
 
-        console.log(order);
+        console.log(order.toJSON());
 
-        const orderTotalPrice = order.Products.reduce((total, p) => {
-            return total + p.price * p.OrderProducts.adet;
-        }, 0);
+        // const orderTotalPrice = order.Products.reduce((total, p) => {
+        //     return total + p.price * p.OrderProducts.adet;
+        // }, 0);
 
-        console.log(orderTotalPrice);
-
-
-        order.totalPrice = orderTotalPrice
-
-        await order.save()
-        //ver2
+        // console.log(orderTotalPrice);
 
 
+        // order.totalPrice = orderTotalPrice
 
-        // const user = await User.findOne({
-        //     where: { id: 'e9213d32-a32e-46f5-a085-6b513129fce1' },
-        //     include: {
-        //         model: Order,
-        //         where: { onay: false },
-        //     },
-        // });
-        // console.log(user.dataValues);
-
-        // user.dataValues.Orders[0].totalPrice = 500;
-
-        // await user.save()
-
+        // await order.save()
+        
         return res.status(200).json(order);
     } catch (error) {
         return res.status(500).json(error.message);
